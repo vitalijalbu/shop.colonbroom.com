fe0bb2bba47a871b41c2ed469956367315d52bb3
vitali <v.jalbu31@gmail.com>
2024-12-02 15:42:23 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./**/*.liquid", "./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '680px',
+        md: '800px'
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '0.5rem',
+          sm: '0',
+          lg: '1rem',
+          xl: '2rem',
+          '2xl': '2rem',
+        },
+      },
+      colors:{
+        primary: '#199a8e',
+        red: '#FE5C5C',
+        bg: '#EAF4F2',
+        disabled: '#A1A8B0',
+      }
+    },
   },
   plugins: [],
 };
